53e388411b83f516de2f6ea153c749afa093c927
Harshnaruka01 <harshnaruka13@gmail.com>
2025-07-25 00:04:11 +0530

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-#Gender == 1 Female   0 Male
-#Churn == 1 Yes   0 No
-# Scaler is exported as scaler.pkl
-# MOdel is exported as model.pkl
-#order of the X == 'Age', 'Gender', 'Tenure', 'MonthlyCharges'
-
 import streamlit as st
 import joblib
 import numpy as np
